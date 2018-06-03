a9f980a624cd61be6c4ea0c9e842e28364411d66
chaimt <cyturel@gmail.com>
2018-06-03 17:40:18 +0300

    enhance data structures

--- app/domain/data_structures.py
+++ app/domain/data_structures.py
@@ -1,14 +1,22 @@
-import json
+import datetime
+
+import logging
+logger = logging.getLogger(__name__)
 
 
 class Message:
 
-    def __init__(self, message_id, message):
+    def __init__(self, message_id, message, username, timestamp):
         self.message_id = message_id
         self.message = message
+        self.username = username
+        self.timestamp = timestamp
+
+    def __eq__(self, other):
+        return self.timestamp == other.timestamp
 
     def to_json(self):
-        return json.dumps({"id": str(self.message_id), "messages": self.message})
+        return str({"id": 123, "messages": self.message, "username": self.username, "timestamp": self.timestamp})
 
 
 class MessageApp:
@@ -17,9 +25,9 @@ class MessageApp:
         self.currentId = 0
         self.messages = {}
 
-    def add_message(self, message):
+    def add_message(self, message, username):
         self.currentId += 1
-        self.messages[str(self.currentId)] = Message(self.currentId, message)
+        self.messages[str(self.currentId)] = Message(self.currentId, message,username, datetime.datetime.utcnow())
         return self.messages[str(self.currentId)]
 
     def peek_message(self, message_id):
--- tests/test_data_structures.py
+++ tests/test_data_structures.py
@@ -1,8 +1,35 @@
-import pytest
-from domain.data_structures import MessageApp
+import unittest
+import datetime
+
+from app.domain.data_structures import MessageApp
+from app.domain.data_structures import Message
 
 
-@pytest.fixture
-def client():
-    messages = MessageApp()
-    assert messages.total_sent() == 0
+class TestMessageApp(unittest.TestCase):
+
+    def test_not_eq(self):
+        message1 = Message(1, "one", "chaim", datetime.datetime.utcnow())
+        message2 = Message(2, "two", "chaim", datetime.datetime.utcnow())
+        self.assertNotEqual(message1, message2)
+
+    def test_eq(self):
+        time = datetime.datetime.utcnow()
+        message1 = Message(1, "one", "chaim", time)
+        message2 = Message(2, "two", "chaim", time)
+        self.assertEqual(message1, message2)
+
+    def test_empty(self):
+        messages = MessageApp()
+        self.assertEqual(messages.total_sent(), 0)
+
+    def test_add(self):
+        messages = MessageApp()
+        message = messages.add_message("test", "chaim")
+        self.assertEqual(message.message_id, 1)
+        self.assertEqual(message.message, "test")
+        self.assertEqual(messages.total_sent(), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
